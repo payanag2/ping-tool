f4293494ef16aa0c2d64ff7b3ebe6fa15011032e
payanag2 <payanag@atomicmail.io>
2026-08-16 10:37:04 +0330

    Bump Android app to v1.1 and trigger APK build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android { namespace = "com.payanag2.pingtool"; compileSdk = 35
-    defaultConfig { applicationId = "com.payanag2.pingtool"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+    defaultConfig { applicationId = "com.payanag2.pingtool"; minSdk = 26; targetSdk = 35; versionCode = 2; versionName = "1.1" }
 }
 
 dependencies {
